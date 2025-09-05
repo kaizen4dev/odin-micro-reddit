3c081c2cdcbd88d61fa2a6a5b72ed565dbe82367
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-09-05 08:06:30 +0300

    Add validations for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: true, length: { in: 3..40 }
+  validates :body, presence: true, length: { maximum: 100000 }
 end
